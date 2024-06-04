6c73ec4672c48ea959f72bca199f325856257d2f
soundaryaBE <soundaryaraj62@gmail.com>
2024-06-04 12:55:16 +0530

    commited in repo

--- src/test/java/fbLoginLogout/LogOutTest.java
+++ src/test/java/fbLoginLogout/LogOutTest.java
@@ -1,5 +1,24 @@
 package fbLoginLogout;
 
-public class LogOutTest {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LogOutTest {
+	@Test
+	public void logout()
+	{
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//String URL=System.getProperty("url");
+		//driver.get(URL);
+		driver.get("https://www.facebook.com/");
+		Reporter.log("loggedout Successfully",true);
+		//hello all
+	}
 }
